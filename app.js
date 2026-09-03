b9c3aa07fc2f1099d21f8108296ceb4d0817ef83
teacherchezka14 <chezkabalicuas1014@gmail.com>
2026-09-04 01:17:05 +0800

    Connect Supabase

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
+// SUPABASE CONNECTION
+const SUPABASE_URL = 'https://quskknnnzrwewgzunrgd.supabase.co';
+const SUPABASE_KEY = 'sb_publishable_J_cs_u7mkJsUnO1GUizi2w_Y5kUalMw';
+
+const supabaseClient = supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_KEY
+);
 const STORE_KEY='teacherChezkaDashboardV1';
 let state=loadState();
 let parsedImportRows=[];
